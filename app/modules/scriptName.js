78f8fe4f105a417146d78f371dad0dc11aa1ae6d
jovellir@gmail.com <721680234b>
2017-07-27 10:59:57 +0600

    first commit

--- app/modules/scriptName.js
+++ app/modules/scriptName.js
@@ -1,8 +1,21 @@
 import $ from 'jquery';
+import 'slick-carousel/slick/slick.min.js';
 
 const scriptName = () => {
 
+    $('.diokan-dropdown-toggle').on('click', function(){
+        $(this).parent().toggleClass('open');
+    });
 
+    $(document).ready(function(){
+        $('.diokan-slider-news').slick({
+            arrows:false,
+            dots:true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+    });
 };
 
 export default scriptName();
